a7f5de09dffa5152ebf1c9dc7fad7aae9af5e576
AlexisL <alexis.larrode@gmail.com>
2020-05-17 21:22:35 +0200

    decoupage de la page index
    creation des fichiers about, awards, education... afin de decouper la page index en plusieurs include

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once('inc/navigation.inc.php') ?>
+
+<?php require_once('inc/about.inc.php') ?>
+
+<?php require_once('inc/experience.inc.php') ?>
+
+<?php require_once('inc/education.inc.php') ?>
+
+<?php require_once('inc/skills.inc.php') ?>
+
+<?php require_once('inc/interests.inc.php') ?>
+
+<?php require_once('inc/awards.inc.php') ?>

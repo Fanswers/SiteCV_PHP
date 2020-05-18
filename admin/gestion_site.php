44048eeb62447863ae81bab029433a9ed6cd7788
AlexisL <alexis.larrode@gmail.com>
2020-05-18 09:35:52 +0200

    Liaison page "skills" à BDD
    Création table "skills" dans la base de donnée.
    Ajout fonctionnalité "ajouter" et "supprimer" skills

--- admin/gestion_site.php
+++ admin/gestion_site.php
@@ -3,15 +3,12 @@ require_once("../inc/init.inc.php");
 
 //--------------------------------- TRAITEMENTS PHP ---------------------------------//
 if(!internauteEstConnecteEtEstAdmin()) header("location:../index.php");
-echo'Admin connecté<br>';
-
 
 //--- SUPPRESSION TABLES ---//
 if(isset($_GET['action']) && $_GET['action'] == "suppressionExperience")
 {   // $contenu .= $_GET['id_produit']
     $resultat = executeRequete("SELECT * FROM experience WHERE id=$_GET[id_produit]");
     $experience_a_supprimer = $resultat->fetch_assoc();
-    $chemin_photo_a_supprimer = $_SERVER['DOCUMENT_ROOT'] . $experience_a_supprimer['photo'];
     if(!empty($experience_a_supprimer['photo']) && file_exists($chemin_photo_a_supprimer)) unlink($chemin_photo_a_supprimer);
     executeRequete("DELETE FROM experience WHERE id=$_GET[id_produit]");
     $contenu .= '<div class="validation">Suppression du produit : ' . $_GET['id_produit'] . '</div>';
@@ -21,9 +18,17 @@ elseif(isset($_GET['action']) && $_GET['action'] == "suppressionEducation")
 {   // $contenu .= $_GET['id_produit']
     $resultat = executeRequete("SELECT * FROM education WHERE id=$_GET[id_produit]");
     $education_a_supprimer = $resultat->fetch_assoc();
-    $chemin_photo_a_supprimer = $_SERVER['DOCUMENT_ROOT'] . $education_a_supprimer['photo'];
     if(!empty($education_a_supprimer['photo']) && file_exists($chemin_photo_a_supprimer)) unlink($chemin_photo_a_supprimer);
     executeRequete("DELETE FROM education WHERE id=$_GET[id_produit]");
+    $contenu .= '<div class="validation">Suppression du produit : ' . $_GET['id_produit'] . '</div>';
+    $_GET['action'] = 'affichage';
+}
+elseif(isset($_GET['action']) && $_GET['action'] == "suppressionSkill")
+{   // $contenu .= $_GET['id_produit']
+    $resultat = executeRequete("SELECT * FROM skills WHERE id=$_GET[id_produit]");
+    $skill_a_supprimer = $resultat->fetch_assoc();
+    if(!empty($skill_a_supprimer['photo']) && file_exists($chemin_photo_a_supprimer)) unlink($chemin_photo_a_supprimer);
+    executeRequete("DELETE FROM skills WHERE id=$_GET[id_produit]");
     $contenu .= '<div class="validation">Suppression du produit : ' . $_GET['id_produit'] . '</div>';
     $_GET['action'] = 'affichage';
 }
@@ -37,6 +42,10 @@ if($_POST and isset($_POST['ajouterExperience']))
 elseif($_POST and isset($_POST['ajouterEducation']))
       {
         $result = $mysqli->query("INSERT INTO education (etablissement, formation, duree, info) VALUES ('$_POST[etablissement]', '$_POST[formation]', '$_POST[duree]', '$_POST[info]')");
+      }
+elseif($_POST and isset($_POST['ajouterSkill']))
+      {
+        $result = $mysqli->query("INSERT INTO skills (info) VALUES ('$_POST[info]')");
       }
 
 
@@ -78,7 +87,7 @@ if(isset($_GET['action']) && $_GET['action'] == "affichage")
             }
         }
         $contenu .= '<td><a href="?action=modificationExperience&id_produit=' . $ligne['id'] .'"><img src="../inc/img/edit.png"></a></td>';
-        $contenu .= '<td><a href="?action=suppressionExperience&id_produit=' . $ligne['id'] .'" OnClick="return(confirm(\'En êtes vous certain ?\'));"><img src="../inc/img/delete.png"></a></td>';
+        $contenu .= '<td><a href="?action=suppressionExperience&id_produit=' . $ligne['id'] .'" OnClick="return(confirm(\'En êtes vous certain ?\'));">Supprimer';
         $contenu .= '</tr>';
     }
     $contenu .= '</table><br><hr><br>';
@@ -112,7 +121,41 @@ if(isset($_GET['action']) && $_GET['action'] == "affichage")
             }
         }
         $contenu .= '<td><a href="?action=modificationEducation&id_produit=' . $ligne['id'] .'"><img src="../inc/img/edit.png"></a></td>';
-        $contenu .= '<td><a href="?action=suppressionEducation&id_produit=' . $ligne['id'] .'" OnClick="return(confirm(\'En êtes vous certain ?\'));"><img src="../inc/img/delete.png"></a></td>';
+        $contenu .= '<td><a href="?action=suppressionEducation&id_produit=' . $ligne['id'] .'" OnClick="return(confirm(\'En êtes vous certain ?\'));">Supprimer';
+        $contenu .= '</tr>';
+    }
+    $contenu .= '</table><br><hr><br>';
+
+    //-- Skills --//
+    $resultat = executeRequete("SELECT * FROM skills");
+     
+    $contenu .= '<h2> Skills </h2>';
+    $contenu .= '<table border="1"><tr>';
+     
+    while($colonne = $resultat->fetch_field())
+    {    
+        $contenu .= '<th>' . $colonne->name . '</th>';
+    }
+    $contenu .= '<th>Modification</th>';
+    $contenu .= '<th>Supression</th>';
+    $contenu .= '</tr>';
+ 
+    while ($ligne = $resultat->fetch_assoc())
+    {
+        $contenu .= '<tr>';
+        foreach ($ligne as $indice => $information)
+        {
+            if($indice == "photo")
+            {
+                $contenu .= '<td><img src="' . $information . '" ="70" height="70"></td>';
+            }
+            else
+            {
+                $contenu .= '<td>' . $information . '</td>';
+            }
+        }
+        $contenu .= '<td><a href="?action=modificationSkill&id_produit=' . $ligne['id'] .'"><img src="../inc/img/edit.png"></a></td>';
+        $contenu .= '<td><a href="?action=suppressionSkill&id_produit=' . $ligne['id'] .'" OnClick="return(confirm(\'En êtes vous certain ?\'));">Supprimer';
         $contenu .= '</tr>';
     }
     $contenu .= '</table><br><hr><br>';
@@ -147,6 +190,13 @@ if(isset($_GET['action']) && $_GET['action'] == "ajout"){
     <label for="info">Description</label><br>
     <input type="text" name="info" placeholder="info" id="info" required=""><br><br>
     <input type="submit" name="ajouterEducation" value="Ajouter education"><br><br>
+    </form>
+
+    <h2>SKILLS</h2>
+    <form method="post">
+    <label for="info">Info</label><br>
+    <input type="text" name="info" placeholder="info" id="info" required=""><br><br>
+    <input type="submit" name="ajouterSkill" value="Ajouter skill"><br><br>
     </form>
     ';
 }

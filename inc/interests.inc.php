044f2b1085dfee2cb9ce82a7d754eb9043138b62
AlexisL <alexis.larrode@gmail.com>
2020-05-19 11:10:25 +0200

    Update interests.inc.php

--- inc/interests.inc.php
+++ inc/interests.inc.php
@@ -1,14 +1,14 @@
     <!-- INTERESTS -->
 
-    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interest">
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
       <div class="w-100">
         <h2 class="mb-5">Interests</h2>
 
         <?php 
 
-        $result = $mysqli->query("SELECT * FROM skills");
+        $result = $mysqli->query("SELECT * FROM interest");
 
-        while($skills = $result->fetch_assoc()){ ?>
+        while($interest = $result->fetch_assoc()){ ?>
         <ul class="fa-ul mb-0">
           <li>
             <i class="fa-li fa fa-check"></i>
